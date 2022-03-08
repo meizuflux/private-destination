001fa2d25791689c286064b46fdfcff25f965e12
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2022-03-08 07:17:58 -0500

    dashboard

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,8 +1,15 @@
+import aiohttp_jinja2
 from app.routing import Controller, view
 from aiohttp import web
 
 class Dashboard(Controller):
     @view()
     class Index(web.View):
+        @aiohttp_jinja2.template("dashboard.html")
         async def get(self):
-            return web.json_response({"hi": "hi"})
+            return {}
+
+    @view("shortner")
+    class Shortner(web.View):
+        async def get(self):
+            return web.json_response({"message": "unfinished"})

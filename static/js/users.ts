fe580cca359e0a1d68abfb2e3a938e59428843cd
meizuflux <meizuflux@outlook.com>
2022-03-22 18:30:12 +0000

    i hate to do this but idk whats going on

--- static/js/users.ts
+++ static/js/users.ts
@@ -1,11 +1,13 @@
 for (let user of document.getElementsByClassName("users-table-row")) {
     const user_id = user.getAttribute("data-id")
+    alert(user_id)
 
     const buttons = user.lastElementChild.firstElementChild as HTMLDivElement
     const authorizeBtn = buttons.firstElementChild
     const deleteBtn = buttons.lastElementChild
 
     authorizeBtn.addEventListener("click", async () => {
+        alert(`/api/users/${user_id}/authorize`)
         const res = await fetch(`/api/users/${user_id}/authorize`)
         user.remove()
     })

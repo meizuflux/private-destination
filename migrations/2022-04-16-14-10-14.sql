a240efc83aed0d0a39a80713545588cb2e9c14f8
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2022-04-16 14:10:45 -0400

    schema for secure notes

---
+++ migrations/2022-04-16-14-10-14.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS notes (
+    id UUID NOT NULL PRIMARY KEY DEFAULT (gen_random_uuid()),
+    owner BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    content BYTEA NOT NULL,
+    has_password BOOLEAN DEFAULT false,
+    share_email BOOLEAN DEFAULT True,
+    private BOOLEAN DEFAULT false,
+    style TEXT DEFAULT 'raw', -- 'raw', 'styled'
+    identifier TEXT DEFAULT 'id', -- 'id', 'name'
+    clicks BIGINT DEFAULT 0 NOT NULL,
+    creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    UNIQUE (owner, name)
+);
--- schema.sql
+++ schema.sql
@@ -27,6 +27,21 @@ CREATE TABLE IF NOT EXISTS urls (
     creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
 );
 
+CREATE TABLE IF NOT EXISTS notes (
+    id UUID NOT NULL PRIMARY KEY DEFAULT (gen_random_uuid()),
+    owner BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    content BYTEA NOT NULL,
+    has_password BOOLEAN DEFAULT false,
+    share_email BOOLEAN DEFAULT True,
+    private BOOLEAN DEFAULT false,
+    style TEXT DEFAULT 'raw', -- 'raw', 'styled'
+    identifier TEXT DEFAULT 'id', -- 'id', 'name'
+    clicks BIGINT DEFAULT 0 NOT NULL,
+    creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    UNIQUE (owner, name)
+);
+
 CREATE OR REPLACE FUNCTION deleteOldSessions() RETURNS TRIGGER AS $$
 BEGIN
     DELETE FROM sessions CASCADE WHERE expires IS NOT NULL AND expires < now() AT TIME ZONE 'utc';

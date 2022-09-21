283c9cfc5bd0d8c85fd8bb36d9854a1d347ddc00
meizuflux <76911441+meizuflux@users.noreply.github.com>
2022-09-21 14:11:37 +0000

    Remove unused JS

--- static/js/textarea.ts
+++
@@ -1,7 +0,0 @@
-for (let textarea of document.getElementsByTagName("textarea")) {
-    textarea.addEventListener("input", function () {
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight+'px';
-        textarea.style.maxHeight = textarea.style.height
-    });
-}
